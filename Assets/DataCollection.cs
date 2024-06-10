2cf89157c4faa46d2054ec3ca391362fbab94a99
Red Williams <brewbrotherred@gmail.com>
2024-06-10 16:58:54 -0700

    add fields and functions for logging data. currently not functional

--- Assets/DataCollection.cs
+++ Assets/DataCollection.cs
@@ -7,30 +7,40 @@ using System.IO;
 public class DataCollection : MonoBehaviour
    
 {
-    [SerializeField] private Text NameInput;
+    // These are currently broken, need to connect
+    [SerializeField] private InputField NameInput;
+    [SerializeField] private InputField NumberInput;
+    [SerializeField] private Dropdown MonthDropdown;
     [SerializeField] private Button TakeDataBtn;
     // Start is called before the first frame update
     void Start()
     {
-        UserData userData = new UserData();
-
+        TakeDataBtn.onClick.AddListener(SaveData);
     }
-
     private class UserData
     {
-        public string NameData;
+        public string Name;
         public int Number;
-        public Selectable Month;
+        public string Month;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void SaveData()
     {
-        if (Input.GetButtonDown("TakeDataBtn"))
+        // should log entry from fields into JSON file
+        UserData userData = new UserData();
         {
-           // Record data in fields to UserData when TakeDataBtn is clicked to JSON file
-
-          // UserData.NameData = NameInput.text;
+           // Name = NameInput.text;
+          //  Number = int.Parse(NumberInput.text);
+         //   Month = MonthDropdown.options[MonthDropdown.value].text;
         }
+
+        string json = JsonUtility.ToJson(userData);
+
+        File.WriteAllText(Application.persistentDataPath + "/UserData.json", json);
+
+        Debug.Log("Data saved to " + Application.persistentDataPath + "/UserData.json");
     }
+
+
+    // later functions should interact with button to show data currently logged in JSON
+    // Search button near the top should allow for user to search by name for number and month data
 }
